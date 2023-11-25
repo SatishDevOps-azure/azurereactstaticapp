9d1c774df0143e62c5af8d3f041893a2d101c647
Satishkumar <50496922+SatishDevOps-azure@users.noreply.github.com>
2023-11-25 10:03:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>DevOps is the combination of cultural philosophies, practices, and tools that increases an organization's ability to deliver applications and services at high velocity: evolving and improving products at a faster pace than organizations using traditional software development and infrastructure management processes. {value}</div>;
 }
 
 export default App;
